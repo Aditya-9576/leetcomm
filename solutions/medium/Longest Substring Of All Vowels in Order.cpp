203981dd395d03993e2413b729ff42ccf33f1f4b
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-01 06:36:17 +0530

    Add solution for Longest Substring Of All Vowels in Order

---
+++ solutions/medium/Longest Substring Of All Vowels in Order.cpp
@@ -0,0 +1,27 @@
+// Title: Longest Substring Of All Vowels in Order
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/longest-substring-of-all-vowels-in-order/
+
+            else {
+                if(k==5)ans=max(ans,count);
+                if(mpp[word[i]]==1){
+            }
+                k=0;count=0;
+        }if(k==5)ans=max(ans,count);
+        return ans;
+
+            else if(mpp[word[i]]==k)count++;
+            }
+                k++;count++;
+            if(mpp[word[i]]==k+1){
+        for(int i=0;i<n;i++){
+        unordered_map<char,int>mpp={{'a',1},{'e',2},{'i',3},{'o',4},{'u',5}};
+        int ans=0,count=0,n=word.size(),k=0;
+    int longestBeautifulSubstring(string word) {
+public:
+class Solution {
+                    k++;count++;
+                }
+    }
+};
