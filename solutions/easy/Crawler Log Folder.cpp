29ce644ae19d885d7f21baa5cfe7b11588a32c5c
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-04 01:46:41 +0530

    Add solution for Crawler Log Folder

---
+++ solutions/easy/Crawler Log Folder.cpp
@@ -0,0 +1,21 @@
+// Title: Crawler Log Folder
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/crawler-log-folder/
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        for(string c:logs){
+        }return ans;
+        int ans=0;
+            if(c=="../" ){
+            
+            else {
+                ans++;
+            }
+    }
+            }
+                if(ans>0)ans--;
+            else if(c=="./");
+};
