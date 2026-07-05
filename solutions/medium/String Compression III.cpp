1b11a2a9bebaaf32aa0e8978cd25de8f0996651c
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-06 01:01:35 +0530

    Add solution for String Compression III

---
+++ solutions/medium/String Compression III.cpp
@@ -0,0 +1,20 @@
+// Title: String Compression III
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/string-compression-iii/
+
+class Solution {
+public:
+    string compressedString(string word) {
+        int n=word.size();string ans="";
+        int u=0;
+        while(u<n){
+            char c=word[u];
+        }return ans;
+            int o=0;
+            while(u<n && c==word[u]){
+            }if(o>0)ans+=to_string(o)+c;
+    }
+                if(o==9){ans+=to_string(9)+c;o=0;}
+                o++;u++;
+};
