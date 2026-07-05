d8a046b98a1e42921dbc4404ff74ed443552ff5c
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-06 01:35:06 +0530

    Add solution for Masking Personal Information

---
+++ solutions/medium/Masking Personal Information.cpp
@@ -0,0 +1,22 @@
+// Title: Masking Personal Information
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/masking-personal-information/
+
+            }if(r==11)ans="+*-"+ans;
+                }
+                    }
+                        ans="***-***-"+ans;
+                    }if(r==4){
+                        ans=s[i]+ans;
+                    r++;if(r<=4 ){
+                if(s[i]<='9'&&s[i]>='0'){
+            for(int i=n-1;i>=0;i--){
+        if(numm){int r=0;
+        }string ans="";
+            if(c=='@'){numm=false;break;}
+        }else {
+
+            if(r==12)ans="+**-"+ans;
+            if(r==13)ans="+***-"+ans;
+            char l=(s[0]>='a')? s[0]:s[0]-'A'+'a';
