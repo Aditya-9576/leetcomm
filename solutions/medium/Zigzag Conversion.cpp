64c03e9baaead33c0484314afffec82bc020db80
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-06 06:26:04 +0530

    Add solution for Zigzag Conversion

---
+++ solutions/medium/Zigzag Conversion.cpp
@@ -0,0 +1,22 @@
+// Title: Zigzag Conversion
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/zigzag-conversion/
+
+        while(u>0){
+            while(i<n){
+        }return ans;
+            int i=0;int h=(numRows-1)*2;
+            u--;
+                if(i%h==j || h-i%h==j){
+            }j++;
+                }i++;
+    }
+                    ans+=s[i];
+        string ans="";
+        if(numRows==1)return s;
+        int n=s.size();int j=0;
+        int u=numRows;
+    string convert(string s, int numRows) {
+public:
+class Solution {
