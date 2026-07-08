25285cb362b03fc47b072a0c4e9a4af0937a1a15
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-08 16:57:35 +0530

    Add solution for Validate IP Address

---
+++ solutions/medium/Validate IP Address.cpp
@@ -0,0 +1,33 @@
+// Title: Validate IP Address
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/validate-ip-address/
+
+                    }
+                        return no;
+                        cout<<"hi";
+                    else {
+                    }
+                        o+=y-'0';
+                    if(y>='0' && y<='9'){
+                    o*=10;
+                for(char y:s){
+                >3)return no;
+                    s+=queryIP[u];u++;
+                }int o=0;cout<<s;if(s.size()>1 && s[0]=='0')return no;if(s.size()==0)return no;if(s.size()
+            ans+="IPv4";
+            int u=0,k=0;
+            while(u<n){
+                string s="";
+                while(u<n && queryIP[u]!='.'){
+        if(queryIP[n-1]=='.'||queryIP[0]=='.'||queryIP[n-1]==':'||queryIP[0]==':')return no;
+        if(isfour){
+        int n=queryIP.size();bool isfour=true;string no="Neither";if(queryIP=="")return no;
+        for(char c:queryIP){
+            if(c==':'){
+                isfour=false;break;
+            }if(c=='.')break;
+        }string ans="";
+class Solution {
+public:
+    string validIPAddress(string queryIP) {
