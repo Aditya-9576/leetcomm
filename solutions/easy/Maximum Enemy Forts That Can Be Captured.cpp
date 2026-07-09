69493f20b2f2e730123c285309c8569301c27784
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-09 14:07:12 +0530

    Add solution for Maximum Enemy Forts That Can Be Captured

--- solutions/easy/Maximum Enemy Forts That Can Be Captured.cpp
+++ solutions/easy/Maximum Enemy Forts That Can Be Captured.cpp
@@ -3,18 +3,4 @@
             // Language: C++
             // Link: https://leetcode.com/problems/maximum-enemy-forts-that-can-be-captured/
 
-            while(u<n&&forts[u]==0){
-                k++;u++;
-            }if(u<n && forts[u]==1){
-            }
-                bac1=true;
-            if(u>0&&forts[u-1]==-1)fro2=true;
-            if(u<n && forts[u]==-1){
-                bac2=true;
-            }
-            if(fro1==true && bac2==true)ans=max(k,ans);
-            u++;
-        }return ans;
-    }
-            if(fro2==true && bac1==true)ans=max(k,ans);
-};
+c
