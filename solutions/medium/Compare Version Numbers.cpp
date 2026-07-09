eb8d50474e6b8763063ed5bd6c5114dead164e0a
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-10 05:05:35 +0530

    Add solution for Compare Version Numbers

---
+++ solutions/medium/Compare Version Numbers.cpp
@@ -0,0 +1,33 @@
+// Title: Compare Version Numbers
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/compare-version-numbers/
+
+            }vec2.push_back(r);
+        }int o=0;
+                r*=10;r+=version2[i]-'0';i++;
+            while(i<m && version2[i]!='.'){
+            long long int r=0;
+        }
+        if(vec1==vec2)return 0;
+            }vec1.push_back(r);
+                r*=10;r+=version1[i]-'0';i++;
+        while(o<vec1.size() && o<vec2.size()){
+            cout<<vec1[o]<<endl;
+            cout<<vec2[o]<<endl;
+            if(vec1[o]>vec2[o])return 1;
+            else if(vec1[o]<vec2[o])return -1;
+            o++;
+        }int h=vec1.size(),g=vec2.size();
+            if(vec1[o]!=0)return 1;
+            o++;
+        }while(o<g){
+            if(vec2[o]!=0)return -1;
+        for(int i=0;i<m;i++){
+            o++;
+        }return 0;
+    }
+        while(o<h){
+            cout<<"dfg"<<vec1[o];
+            cout<<"dfg"<<vec2[o];
+};
