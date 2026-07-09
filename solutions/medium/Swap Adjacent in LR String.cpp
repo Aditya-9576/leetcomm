28173fa647deaea71fceb3dbb3c94dccb2f6682f
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-09 18:41:37 +0530

    Add solution for Swap Adjacent in LR String

---
+++ solutions/medium/Swap Adjacent in LR String.cpp
@@ -0,0 +1,35 @@
+// Title: Swap Adjacent in LR String
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/swap-adjacent-in-lr-string/
+
+                    }
+                        if(v>u)return false;
+                        if(v<u)return false;
+                        while(result[v]!='L'){v++;}
+                        while(v<n && result[v]!='R'){
+                    if(start[u]=='R'){
+                if(start[u]!='X'){
+            while(u<n){
+            u=0;int v=0;
+        }
+                    else if(start[u]=='L'){
+        if(neww1==neww2){
+            u++;
+            if(result[u]!='X')neww2+=result[u];
+            if(start[u]!='X')neww1+=start[u];
+        while(u<n){
+        int u=0;
+        string neww1="",neww2="";
+        int n=start.size();
+    bool canTransform(string start, string result) {
+public:
+class Solution {
+                    }v++;
+                }u++;
+            }
+        }
+        else return false;
+            return true;
+                            v++;
+                        }
