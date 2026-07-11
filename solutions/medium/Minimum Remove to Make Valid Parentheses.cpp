3db46896d63054cd7103a9a0d62fcfcc1b3322a8
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-11 20:51:52 +0530

    Add solution for Minimum Remove to Make Valid Parentheses

--- solutions/medium/Minimum Remove to Make Valid Parentheses.cpp
+++ solutions/medium/Minimum Remove to Make Valid Parentheses.cpp
@@ -3,4 +3,23 @@
             // Language: C++
             // Link: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
 
-c
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int>st;string ans="";
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(c=='('){
+        }while(!st.empty()){
+            else if(c==')'){
+            }cout<<c;
+                if(st.empty())continue;
+                st.push(ans.size());
+            }
+            ans.erase(st.top(),1);st.pop();
+        }return ans;
+    }
+            char c=s[i];
+            ans+=c;
+                else st.pop();
+};
