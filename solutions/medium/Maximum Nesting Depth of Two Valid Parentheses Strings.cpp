6bb9c5d6e07081f18a6d6e012fc371157f4df963
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-11 20:36:56 +0530

    Add solution for Maximum Nesting Depth of Two Valid Parentheses Strings

--- solutions/medium/Maximum Nesting Depth of Two Valid Parentheses Strings.cpp
+++ solutions/medium/Maximum Nesting Depth of Two Valid Parentheses Strings.cpp
@@ -3,4 +3,20 @@
             // Language: C++
             // Link: https://leetcode.com/problems/maximum-nesting-depth-of-two-valid-parentheses-strings/
 
-c
+class Solution {
+public:
+    vector<int> maxDepthAfterSplit(string seq) {
+        int n=seq.size();
+        vector<int>ans(n,0);
+        int u=0,v=0;
+        for(char c:seq){
+            if(c=='('){
+        }return ans;
+                if(u%2==0)ans[u+v]++;
+            }else {
+                if(v%2==0)ans[u+v]++;
+            }
+    }
+                u++;
+                v++;
+};
