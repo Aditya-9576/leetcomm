cc5f68c48a8027c0eb5f5e428d3a93103f0d4763
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-12 06:33:10 +0530

    Add solution for Check If Word Is Valid After Substitutions

---
+++ solutions/medium/Check If Word Is Valid After Substitutions.cpp
@@ -0,0 +1,27 @@
+// Title: Check If Word Is Valid After Substitutions
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/check-if-word-is-valid-after-substitutions/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        for(char c:s){
+            if(c=='c'){
+        }
+            }else result+=c;
+    }
+        string result="";
+                if(result.back()=='b')result.pop_back();
+                else return false;
+                if(result.back()=='a')result.pop_back();
+                else return false;
+        if(result.size()==0)return true;
+                if(result.length()<2)return false;
+        return false;
+};
