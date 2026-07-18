de23614a1ab6726f6bbdd25d4e5aad3c09dfee71
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-19 00:41:11 +0530

    Add solution for Kth Missing Positive Number

--- solutions/easy/Kth Missing Positive Number.cpp
+++ solutions/easy/Kth Missing Positive Number.cpp
@@ -3,24 +3,19 @@
             // Language: C++
             // Link: https://leetcode.com/problems/kth-missing-positive-number/
 
-        if(arr[n-1]-n<k){
-        int u=0;
-        bool kk=false;
-        int r=n-1;
-        int l=0,n=arr.size();
-    int findKthPositive(vector<int>& arr, int k) {
-public:
 class Solution {
-            return k+n;
-        }if(arr[0]>k){
-            return k;
-        }
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int l=0,r=arr.size();
         while(l<r){
             int mid=l+(r-l)/2;
-            if(arr[mid]-mid>k){
+            if(k+mid<arr[mid]){
+        }return r+k;
                 r=mid;
-            }else if(arr[mid]-mid<=k){
+            }else{
                 l=mid+1;
             }
-        }
-        return k+r;
+    }
+        if(k+r>arr[r-1])return k+r;
+        if(k<arr[0])return k;
+};
