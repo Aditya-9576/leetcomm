b47a59562541acdfbff5a740051bd9f3761139f2
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-19 01:12:16 +0530

    Add solution for Search Insert Position

---
+++ solutions/easy/Search Insert Position.cpp
@@ -0,0 +1,20 @@
+// Title: Search Insert Position
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0,r=nums.size();
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)return mid;
+        }return l;
+            else if(nums[mid]<target){
+                l=mid+1;
+            }else {
+                r=mid;
+            }
+    }
+};
