fc7dddb1787bd0f4cc8a49c317461a9554e6fcb0
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-29 18:19:12 +0530

    Add solution for Capacity To Ship Packages Within D Days

---
+++ solutions/medium/Capacity To Ship Packages Within D Days.cpp
@@ -0,0 +1,38 @@
+// Title: Capacity To Ship Packages Within D Days
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+
+        for(auto n:weights){
+            l=max(l,n);
+        }
+            r+=n;
+        while(l<=r){
+            if(isvalid(weights,days,mid)){
+        }return ans;
+            }else{
+    }
+            int mid=l+(r-l)/2;
+    bool isvalid(vector<int>&weights,int days,int mid){
+        int u=0;
+        int vv=0;
+        for(auto n:weights){
+        }
+                if(vv>0)u++;
+            }else{
+                vv+=n;
+            }
+                r=mid-1;
+                l=mid+1;
+            }
+            if(vv+n>mid){
+        if(days>=u){
+                vv=n;
+        if(vv>0)u++;
+        int ans=0;
+                ans=mid;
+        int l=0,r=0;
+    int shipWithinDays(vector<int>& weights, int days) {
+public:
+class Solution {
+            cout<<l<<"df"<<r<<endl;
