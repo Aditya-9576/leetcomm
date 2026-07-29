9334fa613ae8a5040516c9fc47e0515c3cc8012f
Aditya-9576 <152336633+Aditya-9576@users.noreply.github.com>
2026-07-29 12:32:33 +0530

    Add solution for Find First and Last Position of Element in Sorted Array

---
+++ solutions/medium/Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,39 @@
+// Title: Find First and Last Position of Element in Sorted Array
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>=target){
+        }
+            }else{
+                r=mid;
+                l=mid+1;
+            }
+        while(l<r){
+            int mid=l+(r-l+1)/2;
+            if(nums[mid]<=target){
+                l=mid;
+            }else{
+                r=mid-1;
+            }
+        }int b=l;
+        l=0;
+        r=nums.size()-1;
+        int a=l;
+        }if(nums[l]!=target){
+                r=mid-1;
+            }else l=mid;
+            return {-1,-1};
+        }
+            cout<<"dfgd"<<mid<<endl;
+            if(nums[mid]>target){
+        
+        vec.push_back(a);
+            int mid=l+(r-l+1)/2;
+        while(l<r){
+        if(nums.size()==0)return {-1,-1};
+        int l=0,r=nums.size()-1;vector<int>vec;
+        l=0;
+        r=nums.size()-1;
